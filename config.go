d834193f926dc6a9509323466707a3ff2f61bd46
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-03-02 12:42:04 -0500

    remove godotenv dep in favor of config.[mode].json files

--- config.go
+++ config.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/joho/godotenv"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -86,24 +85,10 @@ func (cfg *config) StaleDuration() time.Duration {
 // initConfig pulls configuration from config.json
 func initConfig(mode string) (cfg *config, err error) {
 	cfg = &config{}
-	if _, err = os.Stat("config.json"); !os.IsNotExist(err) {
-		// read config data into a byte slice.
-		var data []byte
 
-		data, err = ioutil.ReadFile("config.json")
-		if err != nil {
-			err = fmt.Errorf("error reading config.json: %s", err)
-			return
-		}
-
-		// unmarshal ("decode") config data into a config struct
-		if err = json.Unmarshal(data, cfg); err != nil {
-			err = fmt.Errorf("error parsing config.json: %s", err)
-			return
-		}
+	if err := loadConfigFile(mode, cfg); err != nil {
+		return cfg, err
 	}
-
-	loadDotEnvFile(mode)
 
 	// override config settings with env settings, passing in the current configuration
 	// as the default. This has the effect of leaving the config.json value unchanged
@@ -158,18 +143,37 @@ func requireConfigStrings(values map[string]string) error {
 	return nil
 }
 
-// checks for .env.[mode] file to read enviornment variables from if the file exists
-func loadDotEnvFile(mode string) {
-	if mode == "" {
-		mode = DEVELOP_MODE
-	}
-	fileName := fmt.Sprintf(".env.%s", mode)
-	if _, err := os.Stat(fileName); err == nil {
-		logger.Printf("loading %s", fileName)
-		if err := godotenv.Load(fileName); err != nil {
-			logger.Println(err.Error())
+// checks for config.[mode].json file to read configuration from if the file exists
+// defaults to config.json, silently fails if no configuration file is present.
+func loadConfigFile(mode string, cfg *config) (err error) {
+	var data []byte
+
+	fileName := fmt.Sprintf("config.%s.json", mode)
+	if !fileExists(fileName) {
+		fileName = "config.json"
+		if !fileExists(fileName) {
+			return nil
 		}
 	}
+
+	data, err = ioutil.ReadFile(fileName)
+	if err != nil {
+		err = fmt.Errorf("error reading %s: %s", fileName, err)
+		return
+	}
+
+	// unmarshal ("decode") config data into a config struct
+	if err = json.Unmarshal(data, cfg); err != nil {
+		err = fmt.Errorf("error parsing %s: %s", fileName, err)
+		return
+	}
+
+	return
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
 }
 
 // outputs any notable settings to stdout
