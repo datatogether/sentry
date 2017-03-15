fb58f0790893db1146278f89f12a87bb3bbe5b06
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-03-15 18:11:19 -0400

    safety commit

--- links.go
+++ links.go
@@ -2,7 +2,7 @@ package main
 
 // ReadDstLinks returns all links that specify a given url as src
 func ReadDstLinks(db sqlQueryable, src *Url) ([]*Link, error) {
-	res, err := db.Query("select urls.url, urls.created, urls.updated, last_get, status, content_type, content_sniff, content_length, title, id, headers_took, download_took, headers, meta, hash from urls, links where links.src = $1 and links.dst = urls.url", src.Url)
+	res, err := db.Query("select urls.url, urls.created, urls.updated, last_head, last_get, status, content_type, content_sniff, content_length, title, id, headers_took, download_took, headers, meta, hash from urls, links where links.src = $1 and links.dst = urls.url", src.Url)
 	if err != nil {
 		return nil, err
 	}
@@ -26,7 +26,7 @@ func ReadDstLinks(db sqlQueryable, src *Url) ([]*Link, error) {
 
 // ReadSrcLinks returns all links that specify a given url as dst
 func ReadSrcLinks(db sqlQueryable, dst *Url) ([]*Link, error) {
-	res, err := db.Query("select urls.url, urls.created, urls.updated, last_get, status, content_type, content_sniff, content_length, title, id, headers_took, download_took, headers, meta, hash from urls, links where links.dst = $1 and links.src = urls.url", dst.Url)
+	res, err := db.Query("select urls.url, urls.created, urls.updated, last_head, last_get, status, content_type, content_sniff, content_length, title, id, headers_took, download_took, headers, meta, hash from urls, links where links.dst = $1 and links.src = urls.url", dst.Url)
 	if err != nil {
 		return nil, err
 	}
