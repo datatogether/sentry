8ad1f1910e10678156de6966468652959e876511
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-06-02 09:22:07 -0400

    update readme

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # Sentry
 ### It watches stuff
 
-- [x] Links-To & Links-From handlers
-- [x] Seed-Url handler
-- [x] Que Handler
-- [x] Stop Handler
-- [x] Mem Stats Handler
-- [x] Sentry PDF, xml, csv, docx, save to S3
-- [x] Add Captures column with hash to follow all get requests
-- [ ] Alerts Column
-- [ ] Setup email Alerts
-- [ ] 200-to-404 alert
+Sentry is a parallelized web crawler written in [Go](https://golang.org) that writes urls, links, & response headers to a Postgres database, then stores the response itself on amazon S3. It keeps a list of “sources”, which use simple string comparison to keep it from wandering outside of designated domains or url paths.
+
+The big difference from other crawlers is a tunable “stale duration”, which will tell the crawler to capture an updated snapshot of the page if the time since the last GET request is older than the stale duration. This gives it a continual “watching” property.
+
+Sentry holds a separate stream of scraping for any url that looks like a dataset. So when it encounters urls that look like `https://foo.com/file.csv`, it assumes that file ending may be a static asset, and places that url on a separate thread for archiving.
 
 # Related Projects
 
--- config.go
+++ config.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/archivers-space/archive"
-	"io/ioutil"
+	conf "github.com/archivers-space/config"
 	"os"
-	"strings"
+	"path/filepath"
 	"time"
 )
 
@@ -19,57 +18,56 @@ const (
 
 // config holds all configuration for the server. It pulls from three places (in order):
 // 		1. environment variables
-// 		2. config.[server_mode].json <- eg: config.test.json
-// 		3. config.json
+// 		2. .[MODE].env OR .env
 //
-// env variables win, but can only set config who's json is ALL_CAPS
-// it's totally fine to not have, say, config.develop.json defined, and just
-// rely on a base config.json. But if you're in production mode & config.production.json
-// exists, that will be read *instead* of config.json.
+// globally-set env variables win.
+// it's totally fine to not have, say, .env.develop defined, and just
+// rely on a base ".env" file. But if you're in production mode & ".env.production"
+// exists, that will be read *instead* of .env
 //
 // configuration is read at startup and cannot be alterd without restarting the server.
 type config struct {
-	Debug bool `json:"DEBUG"`
+	Debug bool
 	// port to listen on, will be read from PORT env variable if present.
-	Port string `json:"PORT"`
+	Port string
 
 	// root url for service
-	UrlRoot string `json:"URL_ROOT"`
+	UrlRoot string
 
 	// url of postgres app db
-	PostgresDbUrl string `json:"POSTGRES_DB_URL"`
+	PostgresDbUrl string
 
 	// Public Key to use for signing metablocks. required.
-	PublicKey string `json:"PUBLIC_KEY"`
+	PublicKey string
 
 	// TLS (HTTPS) enable support via LetsEncrypt, default false
 	// should be true in production
-	TLS bool `json:"TLS"`
+	TLS bool
 
 	// How long before a url is considered stale, in hours.
-	StaleDurationHours time.Duration `json:"stale_duration_hours"`
+	StaleDurationHours time.Duration
 	// crawl urls?
 	Crawl bool
 	// Weather or not the crawler respects robots.txt
 	Polite bool
 	// how long to wait between requests. one day this'll be dynamically
 	// modifiable
-	CrawlDelaySeconds time.Duration `json:"crawl_delay_seconds"`
+	CrawlDelaySeconds time.Duration
 	// Content Types to Store
 	StoreContentTypes []string
 
 	// read from env variable: AWS_REGION
 	// the region your bucket is in, eg "us-east-1"
-	AwsRegion string `json:"AWS_REGION"`
+	AwsRegion string
 	// read from env variable: AWS_S3_BUCKET_NAME
 	// should be just the name of your bucket, no protocol prefixes or paths
-	AwsS3BucketName string `json:"AWS_S3_BUCKET_NAME"`
+	AwsS3BucketName string
 	// read from env variable: AWS_ACCESS_KEY_ID
-	AwsAccessKeyId string `json:"AWS_ACCESS_KEY_ID"`
+	AwsAccessKeyId string
 	// read from env variable: AWS_SECRET_ACCESS_KEY
-	AwsSecretAccessKey string `json:"AWS_SECRET_ACCESS_KEY"`
+	AwsSecretAccessKey string
 	// path to store & retrieve data from
-	AwsS3BucketPath string `json:"AWS_S3_BUCKET_PATH"`
+	AwsS3BucketPath string
 
 	// seed        = flag.String("seed", "", "seed URL")
 	// cancelAfter = flag.Duration("cancelafter", 0, "automatically cancel the fetchbot after a given time")
@@ -83,15 +81,15 @@ type config struct {
 	// username & password that must be passed in with every request.
 	// leaving these values blank will disable http auth
 	// read from env variable: HTTP_AUTH_USERNAME
-	HttpAuthUsername string `json:"HTTP_AUTH_USERNAME"`
+	HttpAuthUsername string
 	// read from env variable: HTTP_AUTH_PASSWORD
-	HttpAuthPassword string `json:"HTTP_AUTH_PASSWORD"`
+	HttpAuthPassword string
 
 	// if true, requests that have X-Forwarded-Proto: http will be redirected
 	// to their https variant
 	ProxyForceHttps bool
 	// CertbotResponse is only for doing manual SSL certificate generation via LetsEncrypt.
-	CertbotResponse string `json:"CERTBOT_RESPONSE"`
+	CertbotResponse string
 }
 
 // StaleDuration turns cfg.StaleDurationHours into a time.Duration
@@ -103,28 +101,16 @@ func (cfg *config) StaleDuration() time.Duration {
 func initConfig(mode string) (cfg *config, err error) {
 	cfg = &config{}
 
-	if err := loadConfigFile(mode, cfg); err != nil {
-		return cfg, err
+	if path := configFilePath(mode, cfg); path != "" {
+		log.Infof("loading config file: %s", filepath.Base(path))
+		if err := conf.Load(cfg, path); err != nil {
+			log.Info("error loading config:", err)
+		}
+	} else {
+		if err := conf.Load(cfg, path); err != nil {
+			log.Info("error loading config:", err)
+		}
 	}
-
-	// override config settings with env settings, passing in the current configuration
-	// as the default. This has the effect of leaving the config.json value unchanged
-	// if the env variable is empty
-	cfg.Debug = readEnvBool("DEBUG", cfg.Debug)
-	cfg.Port = readEnvString("PORT", cfg.Port)
-	cfg.UrlRoot = readEnvString("URL_ROOT", cfg.UrlRoot)
-	cfg.PublicKey = readEnvString("PUBLIC_KEY", cfg.PublicKey)
-	cfg.TLS = readEnvBool("TLS", cfg.TLS)
-	cfg.PostgresDbUrl = readEnvString("POSTGRES_DB_URL", cfg.PostgresDbUrl)
-	cfg.HttpAuthUsername = readEnvString("HTTP_AUTH_USERNAME", cfg.HttpAuthUsername)
-	cfg.HttpAuthPassword = readEnvString("HTTP_AUTH_PASSWORD", cfg.HttpAuthPassword)
-	cfg.AwsAccessKeyId = readEnvString("AWS_ACCESS_KEY_ID", cfg.AwsAccessKeyId)
-	cfg.AwsSecretAccessKey = readEnvString("AWS_SECRET_ACCESS_KEY", cfg.AwsSecretAccessKey)
-	cfg.AwsRegion = readEnvString("AWS_REGION", cfg.AwsRegion)
-	cfg.AwsS3BucketName = readEnvString("AWS_S3_BUCKET_NAME", cfg.AwsS3BucketName)
-	cfg.AwsS3BucketPath = readEnvString("AWS_S3_BUCKET_PATH", cfg.AwsS3BucketPath)
-	cfg.CertbotResponse = readEnvString("CERTBOT_RESPONSE", cfg.CertbotResponse)
-	// cfg.StaleDuration = readEnvInt("STALE_DURATION", cfg.StaleDuration)
 
 	// make sure port is set
 	if cfg.Port == "" {
@@ -147,30 +133,6 @@ func initConfig(mode string) (cfg *config, err error) {
 	return
 }
 
-// readEnvString reads key from the environment, returns def if empty
-func readEnvString(key, def string) string {
-	if env := os.Getenv(key); env != "" {
-		return env
-	}
-	return def
-}
-
-// readEnvBool read key form the env, converting to a boolean value. returns def if empty
-func readEnvBool(key string, def bool) bool {
-	if env := os.Getenv(key); env != "" {
-		return env == "true" || env == "TRUE" || env == "t"
-	}
-	return def
-}
-
-// readEnvString reads a slice of strings from key environment var, returns def if empty
-func readEnvStringSlice(key string, def []string) []string {
-	if env := os.Getenv(key); env != "" {
-		return strings.Split(env, ",")
-	}
-	return def
-}
-
 // requireConfigStrings panics if any of the passed in values aren't set
 func requireConfigStrings(values map[string]string) error {
 	for key, value := range values {
@@ -182,33 +144,21 @@ func requireConfigStrings(values map[string]string) error {
 	return nil
 }
 
-// checks for config.[mode].json file to read configuration from if the file exists
-// defaults to config.json, silently fails if no configuration file is present.
-func loadConfigFile(mode string, cfg *config) (err error) {
-	var data []byte
+func packagePath(path string) string {
+	return filepath.Join(os.Getenv("GOPATH"), "src/github.com/archivers-space/sentry", path)
+}
 
-	fileName := fmt.Sprintf("config.%s.json", mode)
+// checks for .[mode].env file to read configuration from if the file exists
+// defaults to .env, returns "" if no file is present
+func configFilePath(mode string, cfg *config) string {
+	fileName := packagePath(fmt.Sprintf(".%s.env", mode))
 	if !fileExists(fileName) {
-		fileName = "config.json"
+		fileName = packagePath(".env")
 		if !fileExists(fileName) {
-			return nil
+			return ""
 		}
 	}
-
-	log.Infof("reading config file: %s", fileName)
-	data, err = ioutil.ReadFile(fileName)
-	if err != nil {
-		err = fmt.Errorf("error reading %s: %s", fileName, err)
-		return
-	}
-
-	// unmarshal ("decode") config data into a config struct
-	if err = json.Unmarshal(data, cfg); err != nil {
-		err = fmt.Errorf("error parsing %s: %s", fileName, err)
-		return
-	}
-
-	return
+	return fileName
 }
 
 // Does this file exist?
